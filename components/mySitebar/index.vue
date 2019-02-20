142574e955b8cd0e45afdbfd99dd67dae5c60aa8
张宸 <531336750@qq.com>
2019-02-20 13:46:20 +0800

    侧菜单颜色

---
+++ components/mySitebar/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sitebar">
+    <ul class="list">
+      <li class="option"><a href="javascript:void(0);">智慧数据</a></li>
+      <li class="option"><a href="javascript:void(0);">智慧服务</a></li>
+      <li class="option"><a href="javascript:void(0);">小微优势</a></li>
+      <li class="option"><a href="javascript:void(0);">关于我们</a></li>
+      <li class="option"><a href="javascript:void(0);">联系我们</a></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less">
+.sitebar {
+  position: fixed;
+  right: 80px;
+  top: 426px;
+  .list {
+    .option {
+      font-size: 14px;
+      color: #999999;
+      a {
+        display: block;
+        width: 94px;
+        color: #999999;
+        text-align: center;
+        line-height: 44px;
+        &:hover {
+          color: #ffffff;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="container">
-    <div class=""></div>
+    <!-- 侧边导航 -->
+    <my-sitebar></my-sitebar>
+    <div class="banner_box">首页</div>
   </section>
 </template>
 
 <script>
-
+import mySitebar from '@/components/mySitebar/index.vue'
 export default {
+  name:'Home',
+  components:{
+    mySitebar
+  }
 }
 </script>
 
