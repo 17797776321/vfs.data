74516741ed7c439f8547c6aeec55be6470c01041
张宸 <531336750@qq.com>
2019-02-20 10:41:25 +0800

    编写导航

--- components/myNav/index.vue
+++ components/myNav/index.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="my_nav"></div>
+  <div class="my_nav">
+    <div class="width clearfix">
+      <div class="logo_box fl">
+        <img src="../assets/images/logo.png" alt="小微数据">
+      </div>
+      <div class="nav_box fl">
+        <a href="javascript:void(0);"
+           class="option">网站首页</a>
+        <a href="javascript:void(0);"
+           class="option">智慧数据</a>
+        <a href="javascript:void(0);"
+           class="option">解决方案</a>
+        <a href="javascript:void(0);"
+           class="option">关于我们</a>
+      </div>
+      <div class="phone_logo_box fr"></div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: 'MyNav',
-  data() {
+  data () {
     return {}
   }
 }
 </script>
 <style lang="less">
-.my_nav{}
+.my_nav {
+}
 </style>
