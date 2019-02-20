ba143ee97a010215aae7ceb7f0d8ad16a18ba526
张宸 <531336750@qq.com>
2019-02-20 15:18:42 +0800

    智慧服务样式

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,11 @@
     <div class="wisdom_service_box">
       <div class="width">
         <div class="circle_box">
-          <a href="javascript:void(0);">大数据</a>
+          <a href="javascript:void(0);" class="pos1">大数据</a>
+          <a href="javascript:void(0);" class="pos2">提高盈利</a>
+          <a href="javascript:void(0);" class="pos3">吸引顾客</a>
+          <a href="javascript:void(0);" class="pos4">消费数据</a>
+          <a href="javascript:void(0);" class="pos5">人工智能</a>
           <h2 class="cente_text">智慧数据分析</h2>
         </div>
       </div>
@@ -80,23 +84,54 @@ export default {
   }
   .wisdom_service_box{
     width: 1920px;
-    height: 960px;
+    height: 708px;
+    padding-top: 252px;
     background:url('../assets/images/banner2.png') no-repeat;
     background-size: 100% 100%;
     .circle_box{
-      width: 426px;
-      height: 426px;
+      width: 466px;
+      height: 466px;
       background: url('../assets/images/circle1.png') no-repeat;
+      background-size: 100% 100%;
       position: relative;
+      margin: 0 auto ;
       a{
         display: block;
         width: 95px;
         height: 95px;
+        background: url('../assets/images/circle2.png') no-repeat;
+        font-size: 18px;
+        color: #172027;
+        text-align: center;
+        line-height: 95px;
         position: absolute;
+      }
+      .pos1{
+        left: 184px;
+        top: -46px;
+      }
+      .pos2{
+        left: -34px;
+        top: 114px;
+      }
+      .pos3{
+        left: 400px;
+        top: 114px;
+      }
+      .pos4{
+        left: 38px;
+        top: 355px;
+      }
+      .pos5{
+        left: 338px;
+        top: 355px;
       }
       .cente_text{
         font-size: 30px;
         color: #ffffff;
+        text-align: center;
+        padding-top: 208px;
+        line-height: 54px;
       }
     }
   }
