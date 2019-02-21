93c75616345bcbaca06a7d325e3c55a59ee3591e
张宸 <531336750@qq.com>
2019-02-21 14:50:02 +0800

    背景图片设置

--- pages/Wisdom/index.vue
+++ pages/Wisdom/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="page-wisdom">
-    <div class="banenr_box"></div>
+    <div class="banenr_box">
+      <h2 class="content">以智慧数据为基础，立足行业新场景</h2>
+    </div>
+    <div class="box1"></div>
+    <div class="box2"></div>
+    <div class="box3"></div>
+    <div class="box4"></div>
+    <div class="box5"></div>
   </div>
 </template>
 <script>
@@ -16,6 +23,43 @@ export default {
     width: 1920px;
     height: 797px;
     background: url('../../assets/images/wisdombanner.png') no-repeat;
+    background-size: 100% 100%;
+    .content{
+      padding-top: 368px;
+      font-size: 40px;
+      color: #ffffff;
+      text-align: center;
+      line-height: 60px;
+    }
+  }
+  .box1{
+    width: 1920px;
+    height: 631px;
+    background: url('../../assets/images/wisdom1.png') no-repeat;
+    background-size: 100% 100%;
+  }
+  .box2{
+    width: 1920px;
+    height: 698px;
+    background: url('../../assets/images/wisdom2.png') no-repeat;
+    background-size: 100% 100%;
+  }
+  .box3{
+    width: 1920px;
+    height: 700px;
+    background: url('../../assets/images/wisdom3.png') no-repeat;
+    background-size: 100% 100%;
+  }
+  .box4{
+    width: 1920px;
+    height: 700px;
+    background: url('../../assets/images/wisdom4.png') no-repeat;
+    background-size: 100% 100%;
+  }
+  .box5{
+    width: 1920px;
+    height: 700px;
+    background: url('../../assets/images/wisdom5.png') no-repeat;
     background-size: 100% 100%;
   }
 }
