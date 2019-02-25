f02f8dde40b6f2193dda5ac8ae6b97cbca9e05a7
张宸 <531336750@qq.com>
2019-02-25 16:38:21 +0800

    关于我们页面

--- pages/About/index.vue
+++ pages/About/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="page-about">
     <div class="banner_box">
-      <div class="tit">我们一直在路上</div>
-      <div class="des">在科技领域的探索一直从未停歇，在人工智能的研究上一直追求极致，致力于完美的用户体验和服务</div>
+      <div style="padding-top: 338px;">
+        <div class="tit myhidden" ref="banner_box_tit">我们一直在路上</div>
+        <div class="des myhidden" ref="banner_box_des">在科技领域的探索一直从未停歇，在人工智能的研究上一直追求极致，致力于完美的用户体验和服务</div>
+      </div>
     </div>
-    <div class="content_box">
+    <div class="content_box" ref="content_box">
       <div class="width">
-        <div class="box1"></div>
-        <div class="box2">
+        <div class="box1 myhidden" ref="box1"></div>
+        <div class="box2 myhidden" ref="box2">
           <h2 class="tit">河南微风尚科技有限公司</h2>
           <p class="des">河南微风尚科技是一家专业的大数据分析服务公司，大数据分析行业开拓者，为客户提供深度用户行为分析平台、以及专业的咨询服务和行业解决方案，致力于帮助客户实现数据驱动。通过用户精准分群、灵活创建并管理营销活动计划，一键执行计划触达用户，并实时分析活动效果，真正形成自动化、精细化的运营闭环。小微数据立足大数据及用户行为分析的技术与实践前沿，业务现已覆盖以互联网、金融、零售快消、高科技、旅游等为代表的十多个主要行业、并可支持企业多个职能部门。</p>
           <p class="des">我们的客户来自各行各业，为了共同目标，我们在工作上密切配合，从创业型小企业到知名品牌，感谢他们对我们的高要求从不同领域给我们带来的挑战，让我们激情的团队有机会用头脑与智慧不断的给客户带来惊喜。</p>
@@ -26,6 +28,7 @@
   </div>
 </template>
 <script>
+import { ScrollAnimate } from '@/utils/animate.js'
 export default {
   name: 'About',
   data () {
@@ -37,6 +40,14 @@ export default {
         {tit:'6 +',des:'行业标准解决方案'}
       ]
     }
+  },
+  mounted() {
+    new ScrollAnimate(this.$refs.banner_box_tit,'fadeInUp',0)
+    new ScrollAnimate(this.$refs.banner_box_des,'fadeInUp',0)
+
+    let content_box = this.$refs.content_box.offsetTop
+    new ScrollAnimate(this.$refs.box1,'fadeInLeft',content_box)
+    new ScrollAnimate(this.$refs.box2,'fadeInRight',content_box)
   }
 }
 </script>
@@ -49,7 +60,6 @@ export default {
     background-size: 100% 100%;
     text-align: center;
     .tit {
-      padding-top: 338px;
       font-size: 90px;
       color: #fefeff;
       line-height: 114px;
