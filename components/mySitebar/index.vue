83b5d6c122d7b6c1c0440ded50a07bb492edefbc
张宸 <531336750@qq.com>
2019-02-25 15:32:47 +0800

    首页滚动动画

--- components/mySitebar/index.vue
+++ components/mySitebar/index.vue
@@ -3,7 +3,7 @@
     <ul class="list">
       <li class="option" :class="{'active':scrollTop>=0 && scrollTop<960}"><a href="javascript:void(0);" @click="jumpPos('banner_box')">智慧数据</a></li>
       <li class="option" :class="{'active':scrollTop>=960 && scrollTop<2870}"><a href="javascript:void(0);" @click="jumpPos('wisdom_service_box')">智慧服务</a></li>
-      <li class="option" :class="{'active':scrollTop>=2870 && scrollTop<3950}"><a href="javascript:void(0);" @click="jumpPos('advantage_box')">小微优势</a></li>
+      <li class="option" :class="{'active':scrollTop>=2870 && scrollTop<3950}"><a href="javascript:void(0);" @click="jumpPos('project_box')">小微优势</a></li>
       <li class="option" :class="{'active':scrollTop>=3950 && scrollTop<4007}"><a href="javascript:void(0);" @click="jumpPos('about_box')">关于我们</a></li>
       <li class="option" :class="{'active':scrollTop>=4007}"><a href="javascript:void(0);" @click="jumpPos('my-footer')">联系我们</a></li>
     </ul>
