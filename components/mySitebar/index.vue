7aee395e4a9be2c334272eda2356441008210279
张宸 <531336750@qq.com>
2019-02-25 15:50:21 +0800

    添加 侧导航选中动画

--- components/mySitebar/index.vue
+++ components/mySitebar/index.vue
@@ -5,7 +5,7 @@
       <li class="option" :class="{'active':scrollTop>=960 && scrollTop<2870}"><a href="javascript:void(0);" @click="jumpPos('wisdom_service_box')">智慧服务</a></li>
       <li class="option" :class="{'active':scrollTop>=2870 && scrollTop<3950}"><a href="javascript:void(0);" @click="jumpPos('project_box')">小微优势</a></li>
       <li class="option" :class="{'active':scrollTop>=3950 && scrollTop<4007}"><a href="javascript:void(0);" @click="jumpPos('about_box')">关于我们</a></li>
-      <li class="option" :class="{'active':scrollTop>=4007}"><a href="javascript:void(0);" @click="jumpPos('my-footer')">联系我们</a></li>
+      <li class="option" :class="{'active':scrollTop>=4007 && scrollTop<4460}"><a href="javascript:void(0);" @click="jumpPos('my-footer')">联系我们</a></li>
     </ul>
   </div>
 </template>
@@ -40,6 +40,7 @@ export default {
   z-index: 99;
   .list {
     .option {
+      position: relative;
       font-size: 14px;
       a {
         display: block;
@@ -50,6 +51,18 @@ export default {
         &:hover {
           color: #999999;
         }
+      }
+      &::after{
+        content:'';
+        display: block;
+        width: 0;
+        height: 2px;
+        background-color: #999999;
+        position: absolute;
+        left: 32px;
+        bottom: 0;
+        z-index: 999;
+        transition: all .3s;
       }
     }
     .active{
@@ -66,6 +79,7 @@ export default {
         position: absolute;
         left: 32px;
         bottom: 0;
+        opacity: 1;
         z-index: 999;
       }
     }
